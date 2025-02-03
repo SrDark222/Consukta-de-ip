14d4c277bb39d67b4b5ca5d815d037a148bd71dd
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-02-03 00:36:42 -0300

    Update Pegar ip.php

--- Pegar ip.php
+++ Pegar ip.php
@@ -1,23 +1,21 @@
 <?php
-// Captura o IP do visitante
-$ip = $_SERVER['REMOTE_ADDR'];
-
-// Configura o webhook do Discord
-$webhook_url = "https://discord.com/api/webhooks/1279974604622008454/tLibeCCFysq30-iXKEB1jUk0jCagLm6Q6c8ASxPQMbqf-NWACdCVS-PvL_YsL8tvnutB";
-
-// Cria a mensagem a ser enviada
-$data = [
-    "content" => "Novo acesso ao site: " . $ip
+// Captura os dados do usuário
+$userData = [
+    "name" => $_POST['name'],
+    "email" => $_POST['email']
 ];
+
+// URL do webhook
+$webhook_url = "https://example.com/webhook";
 
 // Inicializa a requisição cURL
 $ch = curl_init($webhook_url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json',
-    'Content-Length: ' . strlen(json_encode($data))
+    'Content-Length: ' . strlen(json_encode($userData))
 ]);
 
 // Executa a requisição
